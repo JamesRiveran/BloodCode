2b8f6a1eed184a0d7f04ab0d5a1d001f9888d32f
elmancho0 <granadosgabriel34@gmail.com>
2024-09-07 12:00:50 -0600

    [FEAT] Interpreter
    An interpreter was added to compile what is generated by the tree
    
    Signed-off-by: elmancho0 <granadosgabriel34@gmail.com>

---
+++ BloodCodeCompiler/interpreter/interpreter.py
@@ -0,0 +1,85 @@
+from ..parser.ast import ASTNode, NumberNode, IdentifierNode, BinaryOpNode, StringNode, DeclarationNode, BlockNode, IfStatementNode, LoopNode, FunctionCallNode, RestNode
+class Interpreter:
+    def __init__(self):
+        self.context = {}
+
+    def execute(self, node):
+        if isinstance(node, BlockNode):
+            return self.execute_block(node)
+        elif isinstance(node, DeclarationNode):
+            return self.execute_declaration(node)
+        elif isinstance(node, BinaryOpNode):
+            return self.execute_binary_op(node)
+        elif isinstance(node, NumberNode):
+            return node.value
+        elif isinstance(node, StringNode):
+            return node.value
+        elif isinstance(node, IdentifierNode):
+            return self.context.get(node.name, None)
+        elif isinstance(node, IfStatementNode):
+            return self.execute_if_statement(node)
+        elif isinstance(node, LoopNode):
+            return self.execute_loop(node)
+        elif isinstance(node, FunctionCallNode):
+            return self.execute_function_call(node)
+        elif isinstance(node, RestNode):
+            return None
+        else:
+            raise Exception(f"Nodo no soportado: {type(node)}")
+
+    def execute_block(self, node):
+        result = None
+        for statement in node.statements:
+            result = self.execute(statement)
+        return result
+
+    def execute_declaration(self, node):
+        for identifier in node.identifier_list:
+            value = None
+            if node.expression:
+                value = self.execute(node.expression)
+            self.context[identifier.name] = value
+        return None
+
+    def execute_binary_op(self, node):
+        left_value = self.execute(node.left)
+        right_value = self.execute(node.right)
+        if node.operator == 'PLUS':
+            return left_value + right_value
+        elif node.operator == 'MINUS':
+            return left_value - right_value
+        elif node.operator == 'MULTIPLY':
+            return left_value * right_value
+        elif node.operator == 'DIVIDE':
+            return left_value / right_value
+        elif node.operator == 'ASSIGN':
+            if isinstance(node.left, IdentifierNode):
+                self.context[node.left.name] = right_value
+                return right_value
+            else:
+                raise Exception("Asignación no válida")
+        else:
+            raise Exception(f"Operador no soportado: {node.operator}")
+
+    def execute_if_statement(self, node):
+        condition_value = self.execute(node.condition)
+        if condition_value:
+            return self.execute(node.true_block)
+        elif node.false_block:
+            return self.execute(node.false_block)
+        return None
+
+    def execute_loop(self, node):
+        self.execute(node.init)  # Inicialización
+        while self.execute(node.condition):  # Condición
+            self.execute(node.block)  # Cuerpo del bucle
+            self.execute(node.increment)  # Incremento
+        return None
+
+    def execute_function_call(self, node):
+        if node.identifier == 'PRAY':
+            for expr in node.arguments:
+                result = self.execute(expr)
+                print(result)  # Imprimir la expresión evaluada
+        else:
+            raise Exception(f"Función no soportada: {node.identifier}")
